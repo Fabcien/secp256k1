9448c9636eb8647e1e6fda9b833801db497a8e5e
Fabien <fabcien@gmail.com>
2020-01-08 11:00:23 +0100

    [TRAVIS] Add cmake build to travis

--- contrib/travis/build_cmake.sh
+++ contrib/travis/build_cmake.sh
@@ -3,14 +3,14 @@
 set -ev
 
 if [ "x$HOST" = "xi686-linux-gnu" ]; then
-  export TOOLCHAIN_FILE="-DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Linux32.cmake"
+  export TOOLCHAIN_FILE="-DCMAKE_TOOLCHAIN_FILE=cmake/platforms/Linux32.cmake"
 fi
 
 if [ "x$BIGNUM" = "xno" ]; then
-  export USE_GMP="-DGMP_FOUND=OFF"
+  export USE_GMP="-DGMP_LIBRARY=OFF"
 fi
 
-cmake -GNinja \
+$EXTRAFLAGS cmake -GNinja \
   -DSECP256K1_ECMULT_STATIC_PRECOMPUTATION=$STATICPRECOMPUTATION \
   -DSECP256K1_ENABLE_MODULE_ECDH=$ECDH \
   -DSECP256K1_ENABLE_MODULE_RECOVERY=$RECOVERY \
@@ -19,6 +19,5 @@ cmake -GNinja \
   -DUSE_ASM_X86_64=$ASM \
   $USE_GMP \
   $TOOLCHAIN_FILE \
-  $EXTRAFLAGS
 
 ninja check-secp256k1
