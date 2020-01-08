b76d2914dd9655b59344b1bf0948dcd6b55f9836
Fabien <fabcien@gmail.com>
2020-01-08 10:41:41 +0100

    [TRAVIS] Move the travis build instructions to a script

---
+++ contrib/travis/build_autotools.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ev
+
+if [ -n "$HOST" ]; then
+  export USE_HOST="--host=$HOST";
+fi
+
+if [ "x$HOST" = "xi686-linux-gnu" ]; then
+  export CC="$CC -m32";
+fi
+
+./autogen.sh
+
+./configure \
+  --enable-experimental=$EXPERIMENTAL \
+  --enable-endomorphism=$ENDOMORPHISM \
+  --with-field=$FIELD \
+  --with-bignum=$BIGNUM \
+  --with-scalar=$SCALAR \
+  --enable-ecmult-static-precomputation=$STATICPRECOMPUTATION \
+  --enable-module-ecdh=$ECDH \
+  --enable-module-recovery=$RECOVERY \
+  --enable-module-schnorr=$SCHNORR \
+  --enable-jni=$JNI $EXTRAFLAGS $USE_HOST
+
+make -j2 $BUILD
