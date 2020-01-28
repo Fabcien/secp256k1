90824f926fbed713330fb0ea2398fc2a2fc39ab5
Fabien <fabcien@gmail.com>
2020-01-28 15:12:21 +0100

    CMake: make the GMP bignum support optional
    It is disable by default.

--- travis/build_cmake.sh
+++ travis/build_cmake.sh
@@ -8,10 +8,6 @@ if [ "x$HOST" = "xi686-linux-gnu" ]; then
   TOOLCHAIN_FILE="-DCMAKE_TOOLCHAIN_FILE=../cmake/platforms/Linux32.cmake"
 fi
 
-if [ "x$BIGNUM" = "xno" ]; then
-  USE_GMP="-DGMP_LIBRARY=OFF"
-fi
-
 mkdir -p buildcmake
 pushd buildcmake
 
@@ -25,6 +21,7 @@ ${CMAKE_COMMAND} -GNinja .. \
   -DSECP256K1_ENABLE_MODULE_RECOVERY=$RECOVERY \
   -DSECP256K1_ENABLE_MODULE_SCHNORR=$SCHNORR \
   -DSECP256K1_ENABLE_JNI=$JNI \
+  -DSECP256K1_ENABLE_BIGNUM=$BIGNUM \
   -DUSE_ASM_X86_64=$ASM \
   $USE_GMP \
   $TOOLCHAIN_FILE \
